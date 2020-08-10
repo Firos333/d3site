ad383ccc100f1aa63025e09417851505fc44bfbd
Firos vp <firosvp333@gmail.com>
2020-08-10 22:32:09 +0530

    TEXT HTML ADDED

--- ascii/views.py
+++ ascii/views.py
@@ -7,17 +7,24 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
 import json 
+import os
 
 def index(request):
     def add_item(item):
         context["tags"].append(item)
+
+    with open('HTML_pages.txt') as f:
+        link_list = f.readlines()
+    link_list = [x.strip() for x in link_list] 
+
     size,length2,length3,match2,match3,Heading2, Heading3, headlink2, headlink3= ([] for i in range(9))
-    link_list =["https://firos333.github.io/","https://firos333.github.io/myfile/"]
+
     for i in link_list:
         req = Request(i)
         html_page = urlopen(req)
         soup = BeautifulSoup(html_page, "lxml")
         links = []
+
         for link in soup.findAll('a'):
             links.append(link.get('href'))
         links = [link for link in links if link.startswith('#')]
@@ -45,11 +52,11 @@ def index(request):
                         return all(c in classes for c in target)                                
                     return do_match                                                             
                 match3 = soup.find_all(match_class3(["sect2"]))
-
         for element in match3:
             length3.append(len(str(element)))
         for element in match2:
             length2.append(len(str(element)))
+
     Heading =Heading2 + Heading3
     Headlink = headlink2 + headlink3
     size = length2 + length3
@@ -57,6 +64,7 @@ def index(request):
     cont_reduced= [(x / factor) for x in size]
     Font_size = [( x*1.5 if x<20 else x if 20<x<50 else x*0.65 if 50<x<200 else x*0.4) for x in cont_reduced]
     context={"tags" : [],}
+
     for lnk, txt, siz in zip(Headlink, Heading,Font_size):
         item = {}
         item["text"]= txt
