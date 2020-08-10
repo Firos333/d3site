e4e6b68c696eecb498d8fdb3760c11fb4b4aef1e
Firos vp <firosvp333@gmail.com>
2020-08-10 13:03:29 +0530

    multiple htmls added

--- ascii/views.py
+++ ascii/views.py
@@ -11,41 +11,43 @@ import json
 def index(request):
     def add_item(item):
         context["tags"].append(item)
-    req = Request("https://firos333.github.io/")
-    html_page = urlopen(req)
-    soup = BeautifulSoup(html_page, "lxml")
-    links = []
-    for link in soup.findAll('a'):
-        links.append(link.get('href'))
-    links = [link for link in links if link.startswith('#')]
-    x=[link.replace('#','') for link in links]
     size,length2,length3,match2,match3,text,Heading2, Heading3, headlink2, headlink3= ([] for i in range(10))
-    for link in x:
-        if soup.find('h2', {'id': link}) != None:
-            head = soup.find('h2', {'id': link}).text
-            headlink2.append(link)
-            Heading2.append(head)
-            def match_class2(target):                                                        
-                def do_match(tag):                                                          
-                    classes = tag.get('class', [])                                          
-                    return all(c in classes for c in target)                                
-                return do_match                                                             
-            match2 = soup.find_all(match_class2(["sect1"]))
-        if soup.find('h3', {'id': link}) != None:
-            head = soup.find('h3', {'id': link}).text
-            headlink3.append(link)
-            Heading3.append(head)
-            def match_class3(target):                                                        
-                def do_match(tag):                                                          
-                    classes = tag.get('class', [])                                          
-                    return all(c in classes for c in target)                                
-                return do_match                                                             
-            match3 = soup.find_all(match_class3(["sect2"]))
+    link_list =["https://firos333.github.io/","https://firos333.github.io/"]
+    for x in link_list:
+        req = Request(x)
+        html_page = urlopen(req)
+        soup = BeautifulSoup(html_page, "lxml")
+        links = []
+        for link in soup.findAll('a'):
+            links.append(link.get('href'))
+        links = [link for link in links if link.startswith('#')]
+        x=[link.replace('#','') for link in links]
+        for link in x:
+            if soup.find('h2', {'id': link}) != None:
+                head = soup.find('h2', {'id': link}).text
+                headlink2.append(link)
+                Heading2.append(head)
+                def match_class2(target):                                                        
+                    def do_match(tag):                                                          
+                        classes = tag.get('class', [])                                          
+                        return all(c in classes for c in target)                                
+                    return do_match                                                             
+                match2 = soup.find_all(match_class2(["sect1"]))
+            if soup.find('h3', {'id': link}) != None:
+                head = soup.find('h3', {'id': link}).text
+                headlink3.append(link)
+                Heading3.append(head)
+                def match_class3(target):                                                        
+                    def do_match(tag):                                                          
+                        classes = tag.get('class', [])                                          
+                        return all(c in classes for c in target)                                
+                    return do_match                                                             
+                match3 = soup.find_all(match_class3(["sect2"]))
 
-    for element in match3:
-        length3.append(len(str(element)))
-    for element in match2:
-        length2.append(len(str(element)))
+        for element in match3:
+            length3.append(len(str(element)))
+        for element in match2:
+            length2.append(len(str(element)))
     Heading =Heading2 + Heading3
     Headlink = headlink2 + headlink3
     size = length2 + length3
